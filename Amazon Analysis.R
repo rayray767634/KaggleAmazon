8291fefb42a32f1fa1b1227b55e5d104fb29c88c
Zach Wooten <zacharyashtonwooten@gmail.com>
2023-10-09 13:52:00 -0600

    logistic regression

--- Amazon Analysis.R
+++ Amazon Analysis.R
@@ -18,7 +18,9 @@ library(embed)
 # read in data
 amaz.test <- vroom("amazontest.csv")
 
-amaz.train <- vroom("amazontrain.csv")
+amaz.train <- vroom("amazontrain.csv") %>%
+  mutate_at('ACTION', as.factor)
+
 
 # recipe
 my_recipe <- recipe(ACTION ~ ., data = amaz.train) %>%
@@ -35,5 +37,25 @@ ggplot(data = baked, mapping = aes(x = ACTION)) +
   geom_bar()
 
 
+# logistic regression
+my_mod <- logistic_reg() %>% # type of model
+  set_engine("glm")
+
+amazon_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data = amaz.train) # fit the workflow
+
+amazonlog_predictions <- predict(amazon_workflow,
+                              new_data = amaz.test,
+                              type = "prob") %>%
+  mutate(ACTION = ifelse(.pred_1>.5,1,0)) %>%
+  bind_cols(., amaz.test) %>% #Bind predictions with test data
+  select(id, .pred_1) %>% #Just keep id and pred_1
+  rename(Action=.pred_1) #rename pred1 to Action (for submission to Kaggle)
+  
+
+
+vroom_write(x=amazonlog_predictions, file="./AmazonLogPreds.csv", delim=",")
 
 
