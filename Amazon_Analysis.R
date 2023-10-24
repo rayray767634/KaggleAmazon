1ce76d938f5e7beb27e2cc0943260599aede870e
Zach Wooten <zacharyashtonwooten@gmail.com>
2023-10-24 10:34:30 -0600

    pca

--- Amazon_Analysis.R
+++ Amazon_Analysis.R
@@ -251,3 +251,103 @@ amazon_knn_predictions <- predict(final_wf_knn,
   rename(Action=.pred_1) #rename pred1 to Action (for submission to Kaggle)
 
 vroom_write(x=amazon_knn_predictions, file="./AmazonKNNPreds.csv", delim=",")
+
+
+# principal component dimension reduction
+my_recipe_pca <- recipe(ACTION ~ ., data = amaz.train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_predictors()) %>%
+  step_pca(all_predictors(), threshold = .9)
+
+
+## nb model pca
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes")
+
+nb_wf <- workflow() %>%
+  add_recipe(my_recipe_pca) %>%
+  add_model(nb_model)
+
+# Tune smoothness and Laplace here
+# Set up grid of tuning values
+tuning_grid_nb <- grid_regular(Laplace(),
+                               smoothness(),
+                               levels = 5)
+
+# Set up K-fold CV
+folds <- vfold_cv(amaz.train, v = 5, repeats = 1)
+# Run the CV
+CV_results_nb <-nb_wf  %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid_nb,
+            metrics = metric_set(roc_auc))
+
+# Find best tuning parameters
+bestTune_nb <- CV_results_nb %>%
+  select_best("roc_auc")
+
+# Finalize workflow and predict
+final_wf_nb <- 
+  nb_wf %>%
+  finalize_workflow(bestTune_nb) %>%
+  fit(data = amaz.train)
+
+# predict
+amazon_nb_pca_predictions <- predict(final_wf_nb,
+                                 new_data = amaz.test,
+                                 type = "prob") %>%
+  mutate(ACTION = ifelse(.pred_1>.5,1,0)) %>%
+  bind_cols(., amaz.test) %>% #Bind predictions with test data
+  select(id, .pred_1) %>% #Just keep id and pred_1
+  rename(Action=.pred_1) #rename pred1 to Action (for submission to Kaggle)
+
+vroom_write(x=amazon_nb_pca_predictions, file="./AmazonNBPCAPreds.csv", delim=",")
+
+
+# KNN pca
+
+# knn model
+knn_model <- nearest_neighbor(neighbors = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kknn")
+
+knn_wf <- workflow() %>%
+  add_recipe(my_recipe_pca) %>%
+  add_model(knn_model)
+
+# fit or tune model
+
+tuning_grid_knn <- grid_regular(neighbors(),
+                                levels = 3)
+
+# Set up K-fold CV
+folds <- vfold_cv(amaz.train, v = 3, repeats = 1)
+# Run the CV
+CV_results_knn <-knn_wf  %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid_knn,
+            metrics = metric_set(roc_auc))
+
+# Find best tuning parameters
+bestTune_knn <- CV_results_knn %>%
+  select_best("roc_auc")
+
+# Finalize workflow and predict
+final_wf_knn <- 
+  knn_wf %>%
+  finalize_workflow(bestTune_knn) %>%
+  fit(data = amaz.train)
+
+# predict
+amazon_knn_pca_predictions <- predict(final_wf_knn,
+                                  new_data = amaz.test,
+                                  type = "prob") %>%
+  mutate(ACTION = ifelse(.pred_1>.5,1,0)) %>%
+  bind_cols(., amaz.test) %>% #Bind predictions with test data
+  select(id, .pred_1) %>% #Just keep id and pred_1
+  rename(Action=.pred_1) #rename pred1 to Action (for submission to Kaggle)
+
+vroom_write(x=amazon_knn_pca_predictions, file="./AmazonKNNPCAPreds.csv", delim=",")
